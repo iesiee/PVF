0d5a759c4a64d28c8107fdb94f6d5e541acdf7cc
Oscar Perpiñán <oscar.perpinan@gmail.com>
2014-09-26 20:24:08 +0200

    Fix import of quantregForest to make randomForest available

--- R/plotQR.R
+++ R/plotQR.R
@@ -2,18 +2,19 @@
 ## latticeExtra::layer
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("q1", "q9"))
 
-plotQR <- function(z, vrbl='ground', col=c('black', 'red'),...){
+plotQR <- function(z, vrbl = 'ground', col=c('black', 'red'),...){
     if (is.null(vrbl)){
         p <- xyplot(z[,'q5'],
                     ylim=c(min(z$q1), max(z$q9)),
                     col = col,
-                    key=list(text=list('q5'),
+                    key=list(text=list('Q5'),
                         lines=list(col='black'),
                         corner = c(0, 0),
                         x = 0.8, y = 0.8))
     } else {
         p <- xyplot(z[,c('q5', vrbl)],
                     auto.key = list(
+                        text = c('Q5', vrbl),
                         corner = c(0, 0),
                         x = 0.8, y = 0.8),
                     ylim=c(min(z$q1), max(z$q9)),
