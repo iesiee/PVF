7ef351b98439af8d428d58c3ce20129033d251bb
Oscar Perpiñán Lamigueiro <oscar@OPL-EUITI.upm.es>
2014-10-26 18:20:43 +0100

    Modify roseDiagram to adapt it to xyplot interface

--- R/rose.R
+++ R/rose.R
@@ -1,49 +1,47 @@
-roseDiagram <- function(dats, ...){
-    nms <- rownames(dats)
-    N <- nrow(dats)
-    dats[N+1, ] <- dats[1,]
+roseDiagram <- function(formula, data, ...){
 
-    theta <- seq(0, 2*pi, length = N+1)
-
-    X <- dats * cos(theta)
-    X <- stack(X)
-    Y <- dats * sin(theta)
-    Y <- stack(Y)
-
-    xyAxis <- cbind(cos(theta), sin(theta))
-
-    radius <- seq(.1, 1, by = .1)
-    circle <- expand.grid(theta = seq(0, 2*pi, length = 100),
-                          r = radius)
-    circle$x <- with(circle, r * sin(theta))
-    circle$y <- with(circle, r * cos(theta))
-
-    xyLim <- 1.15
-    xyNames <- cbind(0.95 * xyLim * cos(theta[-(N+1)]),
-                     0.95 * xyLim * sin(theta[-(N+1)]))
-
-    XY <- data.frame(x = X$values, y = Y$values, ind = X$ind)
-
-    xyplot(y ~ x, data = XY, groups = ind,
-           xlim = c(-1, 1)*xyLim*1.1, ylim = c(-1, 1)*xyLim*1.1,
+    xyplot(formula, data = data, 
+           xlim = c(-1, 1) * 1.1, ylim = c(-1, 1) * 1.1,
            xlab = '', ylab = '',
            type = 'b', pch = 21, aspect = 'iso',
-           scales=list(draw = FALSE),
-           panel = function(...){
+           scales = list(draw = FALSE),
+           panel = function(x, y, ...){
+               x <- factor(x)
+               nms <- levels(x)
+               N <- nlevels(x)
+
+               y <- c(y, y[1])
+               theta <- seq(0, 2*pi, length = N+1)
+
+               xtheta <- y * cos(theta)
+               ytheta <- y * sin(theta)
+
                ## Axis
+               xyAxis <- cbind(cos(theta), sin(theta))
+
                apply(xyAxis, 1, FUN = function(xy){
                    panel.lines(c(0, xy[1]), c(0, xy[2]),
                                lwd = .5, col = 'darkgray')
                })
                ## Circles
+               radius <- seq(.1, 1, by = .1)
+               circle <- expand.grid(theta = seq(0, 2*pi, length = 100),
+                                     r = radius)
+               circle$x <- with(circle, r * sin(theta))
+               circle$y <- with(circle, r * cos(theta))
+
                panel.xyplot(circle$x, circle$y,
                             lty = 2, type = 'l',
                             col = 'darkgrey')
                panel.text(c(0, .5, 1), 0, labels = c(0, .5, 1),
                           pos = 1, cex = 1)
                ## Data
-               panel.xyplot(...)
+               panel.xyplot(xtheta, ytheta, ...)
                ## Names of variables
+               xyLim <- 1.15
+               xyNames <- cbind(0.95 * xyLim * cos(theta[-(N+1)]),
+                                0.95 * xyLim * sin(theta[-(N+1)]))
+               
                panel.text(xyNames[,1], xyNames[,2], nms)
            },
            ...)
