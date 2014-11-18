608b7f780b392377b3f6392589a30864bfba1042
Oscar Perpiñán Lamigueiro <oscar@OPL-EUITI.upm.es>
2014-11-18 12:24:18 +0100

    targetDiagram accepts type (quantile and at) and cuts

--- R/targetDiagram.R
+++ R/targetDiagram.R
@@ -4,6 +4,8 @@ targetDiagram <- function(data, class = '',
                           xlab = expression("RMSEc"%.%"sign("*sigma^"*"*")"),
                           ylab = 'MBE',
                           auto.key = list(space = 'right'),
+                          scales = list(cex = 0.6),
+                          type = 'quantile', cuts = seq(0.25, 1, .25),
                           ...){
     
     ## Dismiss values of normalized RMSE above 1
@@ -14,7 +16,12 @@ targetDiagram <- function(data, class = '',
     data$difSD <- with(data, sdm - sdo)
 
     ## Quantile Circles
-    radius <- quantile(data$nrmse, probs = seq(.25, 1, .25), na.rm = TRUE)
+    radius <- switch(type,
+                     quantile = {
+                             quantile(data$nrmse, probs = cuts, na.rm = TRUE)
+                     },
+                     at = cuts)
+
     circle <- expand.grid(theta = seq(0, 2*pi,length = 100),
                           r = radius)
     circle$x <- with(circle, r * sin(theta))
@@ -25,24 +32,26 @@ targetDiagram <- function(data, class = '',
                            ifelse(class == '', '', paste('|', class))
                            )
                      )
-
+    
     xyplot(ff, 
            data = data,
            circle = circle,
            xlab = xlab, ylab = ylab,
-           aspect='iso', scales = list(x = list(draw = FALSE)),
+           aspect='iso', scales = scales,
+           xlim = extendrange(circle$x),
+           ylim = extendrange(circle$y),
            auto.key = auto.key,
            panel = function(..., circle){
                ## Vertical and Horizontal Axis
-               panel.abline(h=0,v=0,lty=2,col='gray')
-               ## Quantile circles
-               panel.xyplot(circle$x, circle$y,
-                            lty = 2, type = 'l', col = 'darkgrey')
-               ## Labels of circles
-               panel.text(0, -radius, labels = signif(radius, 1),
-                          pos = 4, cex = 0.6)
+               panel.abline(h=0,v=0, lwd = 0.6, col='gray')
                ## Data
                panel.xyplot(...)
+               ## Quantile circles
+               panel.xyplot(circle$x, circle$y,
+                            lwd = 0.6, type = 'l', col = 'darkgrey')
+               ## Labels of circles
+               panel.text(0, -radius, labels = signif(radius, 2),
+                          pos = 1, cex = scales$cex)
            }, ...)
 
 }
