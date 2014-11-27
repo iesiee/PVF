78a82e927228ded54657dfc2b277747989eae315
Oscar Perpiñán <oscar.perpinan@gmail.com>
2014-11-27 16:52:48 +0100

    hour, hourHalf not exported

--- R/misc.R
+++ R/misc.R
@@ -1,5 +1,10 @@
+## Dismiss minutes and seconds
+## 01:22:24 --> 01:00:00
+## 01:32:24 --> 01:00:00
 hour <- function(tt)as.POSIXct(trunc(tt, 'hours'))
-
+## Idem, but centering
+## 01:22:24 --> 01:00:00
+## 01:32:24 --> 02:00:00
 hourHalf <- function(tt)as.POSIXct(trunc(tt+30*60, 'hours'))
-
+## Dismiss seconds
 minute <- function(tt)as.POSIXct(trunc(tt, 'mins'))
