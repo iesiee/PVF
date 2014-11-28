fd90945fb5072afbfa0a2a413ae03514730369b5
Oscar Perpiñán <oscar.perpinan@gmail.com>
2014-11-28 09:37:29 +0100

    Include sun geometry in extractForecast and predVarsLocal

--- R/extractForecast.R
+++ R/extractForecast.R
@@ -1,50 +1,72 @@
 extractForecast <- function(point, seqDays, vrbl,
                             src = 'meteogalicia',
                             remote = TRUE, dataDir = '.',
+                            sun = FALSE,
                             ...){
-  
-  fl <- lapply(seqDays, FUN=function(d){
+    if (!is(point, "SpatialPoints")) {
+        ## Some of next methods use spTransform, that works only with
+        ## SpatialPoints.
+        ## Assume long-lat projection
+        projLL <- '+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0'
+        point <- SpatialPoints(cbind(point[1], point[2]),
+                               CRS(projLL))
+    }
     
-    message(paste(d, vrbl, sep=' '))
-    
-    ### Extract forecast using only the first frames from each run
-    forecastDay <- try(getRasterDays(var = vrbl, start = d, end = d, remote = remote,
-                                    service = src, dataDir = dataDir,...),
-                       silent=FALSE)
-    
-    if(class(forecastDay) != 'try-error'){
-       ### Value at the location
-       vals <- as.vector(extract(forecastDay, point))
-       ### Spatial interpolation with IDW
-       idwVals <- sapply(unstack(forecastDay),
-                         idwNear, point=point, nmax=25)
-       ### Spatial variability with terrain indexes
-       terrIdx <- t(sapply(unstack(forecastDay),
-                           terrainIndex, point))
-       ### Combine vals, idwVals and terrIdx
-       zoo(data.frame(point=vals, idw=idwVals, terrIdx),
-           getZ(forecastDay))
-     } else {
-       ### Bad files result in the elimination of the corresponding day
-       NULL
-     }
+    fl <- lapply(seqDays, FUN=function(d){
         
-  })
+        message(paste(d, vrbl, sep=' '))
+        
+        ## Extract forecast using only the first frames from each run
+        forecastDay <- try(getRasterDays(var = vrbl,
+                                         start = d, end = d,
+                                         remote = remote,
+                                         service = src,
+                                         dataDir = dataDir,
+                                         ...),
+                           silent=FALSE)
+        
+        if(class(forecastDay) != 'try-error'){
+            ## Value at the location
+            vals <- as.vector(extract(forecastDay, point))
+            ## Spatial interpolation with IDW
+            idwVals <- sapply(unstack(forecastDay),
+                              idwNear, point=point, nmax=25)
+            ## Spatial variability with terrain indexes
+            terrIdx <- t(sapply(unstack(forecastDay),
+                                terrainIndex, point))
+            ## Combine vals, idwVals and terrIdx
+            zoo(data.frame(point=vals, idw=idwVals, terrIdx),
+                getZ(forecastDay))
+        } else {
+            ## Bad files result in the elimination of the
+            ## corresponding day
+            NULL
+        }
+    })
+    
+    ## Value at the point and spacial variability with terrain indexes
+    spaceIdx <- do.call(rbind, fl)
+    attr(index(spaceIdx), "tzone") <- "UTC"
   
-  ### Value at the point and spacial variability with terrain indexes
-  spaceIdx <- do.call(rbind, fl)
+    ## Temporal variability with temporal index
+    forecastRuns <- getPointRuns(point, var = vrbl,
+                                 start = seqDays[1],
+                                 end = seqDays[length(seqDays)],
+                                 service = src)
+    tempIdx <- tempIndex(forecastRuns)
+    tempIdx <- zoo(data.frame(timeDesv=tempIdx),
+                   index(forecastRuns))
+    
+    ##  Combine value at the point and spacial and temporal
+    ##  variabilities
+    forecast <- merge(spaceIdx, tempIdx)
+    forecast <- na.omit(forecast)
+    attr(index(forecast), "tzone") <- "UTC"
   
-  ### Temporal variability with temporal index
-  forecastRuns <- getPointRuns(point, var = vrbl,
-                               start = seqDays[1],
-                               end = seqDays[length(seqDays)],
-                               service = src)
-  tempIdx <- tempIndex(forecastRuns)
-  tempIdx <- zoo(data.frame(timeDesv=tempIdx),
-                 index(forecastRuns))
-  
-  ### Combine value at the point and spacial and temporal variabilities
-  forecast <- merge(spaceIdx, tempIdx)
-  forecast <- na.omit(forecast)
-  
+    ## Do we need to add sun geometry?
+    if (isTRUE(sun)){
+        sunGeometry(point, forecast)
+    } else {
+        forecast
+    }
 }
--- R/misc.R
+++ R/misc.R
@@ -1,3 +1,36 @@
+## Extract lonlat from a point
+point2lonlat <- function(point){
+    if (is(point, "SpatialPoints")) {
+        if (!isLonLat(point)) {
+            if (require(rgdal, quietly = TRUE)) 
+                point <- spTransform(point,
+                                     CRS("+proj=longlat +ellps=WGS84"))
+            else stop("`rgdal` is needed if `point` is projected.")
+        }
+        lon <- coordinates(point)[1]
+        lat <- coordinates(point)[2]
+        return(c(lon, lat))
+    }
+    else {
+        return(point)
+    }
+}
+
+sunGeometry <- function(point, forecast){
+    ## Sun geometry
+    lonlat <- point2lonlat(point)
+    sol <- calcSol(lat = lonlat[2],
+                   BTi=local2Solar(index(forecast), lon=lonlat[1]))
+    sol <- as.zooI(sol)[, c('AlS', 'AzS', 'Bo0')]
+    sol$Bo0[is.na(sol$Bo0)] <- 0
+    index(sol) <- index(forecast)
+    
+    ## Merge and Time Zone
+    forecast <- merge(forecast, sol)
+    attr(index(forecast), "tzone") <- "UTC"
+    forecast
+}
+
 ## Dismiss minutes and seconds
 ## 01:22:24 --> 01:00:00
 ## 01:32:24 --> 01:00:00
